1e785a2bd324cff9ccdc7e0245d4dbc95d47603a
Cside <cside.story@gmail.com>
2023-02-09 14:07:29 +0900

    Feature: Tooltip

--- src/components/LocalResourceLink.tsx
+++ src/components/LocalResourceLink.tsx
@@ -4,19 +4,16 @@ import { handleClickLocalResource } from '../utils';
 export const LocalResourceLink = ({
   href,
   target,
-  title,
   children,
 }: {
   href: string | (() => string);
   target?: '_blank';
-  title?: string;
   children: ReactNode;
 }) => {
   return (
     <a
       href="#"
       title="title"
-      {...(title && { title })}
       onClick={(event) => {
         return handleClickLocalResource({
           url: typeof href === 'function' ? href() : href,
